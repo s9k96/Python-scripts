63bb8688dbc8209b74c61019f2512513e8eb130f
Shubham Saxena <shubhamsaxena915@gmail.com>
2016-07-08 22:21:44 +0530

    Open Video Camera.

---
+++ load_video.py
@@ -0,0 +1,13 @@
+import numpy as np
+import cv2
+
+cap = cv2.VideoCapture(0)
+while(True):
+    ret,frame = cap.read()
+    gray = cv2.cvtColor(frame,1)
+    # print ret
+    cv2.imshow('frame',gray)
+    if cv2.waitKey(1) & 0xFF == ord('q'): 
+        break
+cap.release()
+cv2.destroyAllWindows()
