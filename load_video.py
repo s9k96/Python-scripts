59480da79071524f03ac81b0d727596e6bda4ee5
Shubham Saxena <shubhamsaxena915@gmail.com>
2016-07-08 22:30:17 +0530

    Update load_video.py
    Open Video Camera.

--- load_video.py
+++ load_video.py
@@ -6,7 +6,8 @@ while(True):
     ret,frame = cap.read()
     gray = cv2.cvtColor(frame,1)
     # print ret
-    cv2.imshow('frame',gray)
+    cv2.imshow('frame1',gray)
+    cv2.imshow('frame2',frame)
     if cv2.waitKey(1) & 0xFF == ord('q'): 
         break
 cap.release()
