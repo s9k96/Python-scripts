b26294e13c68115ebd83976d3a00f88b1fa83506
Shubham Saxena <shubhamsaxena915@gmail.com>
2016-07-08 22:03:16 +0530

    Load an image

--- load.py
+++ load.py
@@ -6,6 +6,3 @@ cv2.imshow('image',img)
 k = cv2.waitKey(0)
 if k == 27:        
     cv2.destroyAllWindows()
-# elif k == ord('s'):
-#     cv2.imwrite('messigray.png',img)
-#     cv2.destroyAllWindows()
